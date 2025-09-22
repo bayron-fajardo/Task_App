d1a13ce4f10b69f2fd3464b118093bca5a593a90
Bayron Fajardo <bayronfajardor@gmail.com>
2025-09-22 00:58:46 -0500

    Model añadido

--- task_app/models.py
+++ task_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class TaskData(models.Model):
+    data = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.data} - {self.created_at.strftime('%d-%m-%Y %H:%M')}"
